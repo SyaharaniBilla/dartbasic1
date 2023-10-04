774c7247d2b9ff90de578e4ef053ad21863f876b
SyaharaniBilla <101703780+SyaharaniBilla@users.noreply.github.com>
2023-10-04 13:26:56 +0700

    Add files via upload

---
+++ lib/soal4.dart
@@ -0,0 +1,14 @@
+void main() {
+  var a = 5;
+  var b = 10;
+
+  var multiplicationResult = a * b;
+  var additionResult = a + b;
+  var subtractionResult = a - b;
+  var divisionResult = a / b;
+
+  print('a * b = $multiplicationResult');
+  print('a + b = $additionResult');
+  print('a - b = $subtractionResult');
+  print('a / b = $divisionResult');
+}
